d236be79e7c8229e5c0df172445727754304f3d7
Coderfaal <fnargund4@gmail.com>
2025-12-26 11:19:37 +0000

    space

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { OtpComponent } from './otp/otp.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component'; 
+
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
